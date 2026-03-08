96ff504ad9bf3c7ebec2c1e934ef792959841f2b
ISHTARR2025 <orellanacar@gmail.com>
2026-03-08 02:11:45 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,16 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-```
-
-**4.** Haz clic en **"Commit new file"**
-
----
-
-**5.** Ahora crea otro archivo:
-
-**"Add file"** → **"Create new file"**
-
-Escribe:
-```
-src/App.jsx
