810f381fc137380389a4269ddc375e3225f98de5
ISHTARR2025 <orellanacar@gmail.com>
2026-03-08 01:57:39 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+```
+
+**4.** Haz clic en **"Commit new file"**
+
+---
+
+**5.** Ahora crea otro archivo:
+
+**"Add file"** → **"Create new file"**
+
+Escribe:
+```
+src/App.jsx
